538d04009f7de8ced91e6c2f89af0a7d215c46bf
Crodi <Crodis@yandex.ru>
2026-07-10 06:11:59 +0300

    fix: change algo

--- stats/analyzer/src/main/java/ru/yandex/practicum/ewm/stats/analyzer/service/UserActionServiceImpl.java
+++ stats/analyzer/src/main/java/ru/yandex/practicum/ewm/stats/analyzer/service/UserActionServiceImpl.java
@@ -31,6 +31,10 @@ public class UserActionServiceImpl implements UserActionService {
     }
 
     private void updateAction(UserAction ua, UserActionAvro avro) {
+        double currentWeight = ActionWeight.getWeight(ua.getActionType());
+        double newWeight = ActionWeight.getWeight(avro.getActionType());
+        if (newWeight <= currentWeight) return;
+
         ua.setActionType(userActionMapper.toActionType(avro.getActionType()));
         ua.setTimestamp(avro.getTimestamp());
         userActionRepository.save(ua);
